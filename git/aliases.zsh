dabeb27df5246a6a5a6685514730c7e2fcf56678
David Yun <abookyun@gmail.com>
2023-09-14 22:35:24 +0800

    add gitdir/config, ignore, aliases,...

---
+++ git/aliases.zsh
@@ -0,0 +1,17 @@
+alias ga='git add'
+alias gac='git add -A && git commit -m'
+
+alias gb='git branch'
+
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+
+alias gd='git diff --color'
+
+alias gl='git pull --prune'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+
+alias gp='git push origin HEAD'
+
+alias gs='git status -sb'
