fca3e12d826141f3d8fd09bfbcc5b9846deccf65
David Yun <abookyun@gmail.com>
2023-09-15 09:44:05 +0800

    customize zsh/files; blend old .zshrc in

---
+++ zsh/env.zsh
@@ -0,0 +1,11 @@
+export LC_ALL=en_US.UTF-8
+
+export EDITOR='vim'
+export VISUAL='vim'
+
+HISTFILE=${XDG_CONFIG_HOME}/zsh/.zsh_history
+HISTSIZE=10000
+SAVEHIST=10000
+HIST_STAMPS="yyyy-mm-dd"
+
+export KEYTIMEOUT=1
--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,13 +1,7 @@
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
-
-fpath=($ZSH/functions $fpath)
-
-autoload -U $ZSH/functions/*(:t)
-
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+# Directory Stack
+setopt AUTO_PUSHD # push the current directory visited on the stack
+setopt PUSHD_IGNORE_DUPS # do not store duplicates in the stack
+setopt PUSHD_SILENT # do not print the directory stack after pushd or popd
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
@@ -30,10 +24,3 @@ setopt HIST_REDUCE_BLANKS
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
-
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
-bindkey '^[[5D' beginning-of-line
-bindkey '^[[5C' end-of-line
-bindkey '^[[3~' delete-char
-bindkey '^?' backward-delete-char
