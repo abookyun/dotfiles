7a2ab4e178c1f8a245e5c09cf4c2bc97c0e8c5da
David Yun <abookyun@gmail.com>
2023-09-16 17:27:53 +0800

    move _history files $XDG_STATE_HOME

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,7 +3,8 @@ export LC_ALL=en_US.UTF-8
 export EDITOR='vim'
 export VISUAL='vim'
 
-HISTFILE=${XDG_CONFIG_HOME}/zsh/.zsh_history
+mkdir -p ${XDG_STATE_HOME}/zsh
+HISTFILE=${XDG_STATE_HOME}/zsh/zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 HIST_STAMPS="yyyy-mm-dd"
