d9232180cd815fd5cc6f1f41fb18f2004c1eaf66
David Yun <abookyun@gmail.com>
2023-09-17 09:27:07 +0800

    enable zsh vi mode

--- zsh/keys.zsh
+++ zsh/keys.zsh
@@ -6,3 +6,6 @@ zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 bindkey "^[[A" up-line-or-beginning-search # Up
 bindkey "^[[B" down-line-or-beginning-search # Down
+
+bindkey -v
+export KEYTIMEOUT=1
