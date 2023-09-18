6b0ae03b5fa18884ecb60d2e54410f855fd25489
David Yun <abookyun@gmail.com>
2023-09-18 10:57:10 +0800

    complete with menu and enable vi move

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,11 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+zstyle ':completion:*' menu select
+
+zmodload zsh/complist
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
