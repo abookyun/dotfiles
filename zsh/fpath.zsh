4044bdeeecc8750fd761162f3b7a7769f0e6c678
David Yun <abookyun@gmail.com>
2023-09-20 12:34:55 +0800

    function convcc to iconv then opencc subtitles

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,4 @@
 # add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+
+autoload -U $DOTFILES/functions/*(:t)
