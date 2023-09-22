b83b035c9e8ca53cb8888b8fb9f75bb778357c14
David Yun <abookyun@gmail.com>
2023-09-22 18:13:58 +0800

    xdg: change rubygems bundler user home

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -7,5 +7,6 @@ if ! [ -d "$RUBY_STATE_HOME" ]; then mkdir -p $RUBY_STATE_HOME; fi
 
 export IRBRC=$XDG_CONFIG_HOME/ruby/irbrc
 export PRYRC=$XDG_CONFIG_HOME/ruby/pryrc
+export BUNDLE_USER_HOME=$RUBY_DATA_HOME/bundle
 
 if (( $+commands[rbenv] )); then eval "$(rbenv init - zsh)"; fi
