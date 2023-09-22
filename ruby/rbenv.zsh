09ccb9b0fffc12304dbf43c914748f04899ca55d
David Yun <abookyun@gmail.com>
2023-09-22 18:36:53 +0800

    rename ruby env variables, less out of zshenv

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,12 +1,12 @@
 export RBENV_ROOT=$XDG_DATA_HOME/rbenv
 
+RUBY_CONFIG_HOME=$XDG_CONFIG_HOME/ruby
 RUBY_DATA_HOME=$XDG_DATA_HOME/ruby
-RUBY_STATE_HOME=$XDG_STATE_HOME/ruby
+if ! [ -d "$RUBY_CONFIG_HOME" ]; then mkdir -p $RUBY_CONFIG_HOME; fi
 if ! [ -d "$RUBY_DATA_HOME" ]; then mkdir -p $RUBY_DATA_HOME; fi
-if ! [ -d "$RUBY_STATE_HOME" ]; then mkdir -p $RUBY_STATE_HOME; fi
 
-export IRBRC=$XDG_CONFIG_HOME/ruby/irbrc
-export PRYRC=$XDG_CONFIG_HOME/ruby/pryrc
+export IRBRC=$RUBY_CONFIG_HOME/irbrc
+export PRYRC=$RUBY_CONFIG_HOME/pryrc
 export BUNDLE_USER_HOME=$RUBY_DATA_HOME/bundle
 
 if (( $+commands[rbenv] )); then eval "$(rbenv init - zsh)"; fi
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -8,3 +8,7 @@ HISTFILE=${XDG_STATE_HOME}/zsh/zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 SHELL_SESSIONS_DISABLE=1
+
+# less
+export LESSHISTFILE="${XDG_STATE_HOME}/less/lesshst"
+mkdir -p $(dirname $LESSHISTFILE)
