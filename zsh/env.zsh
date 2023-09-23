b33d5065e1d0ef2e73b7bfa1bf99b003594f0224
David Yun <abookyun@gmail.com>
2023-09-23 17:38:46 +0800

    define my_vim_* env to avoid path expand in vimrc

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,15 @@
 export LC_ALL=en_US.UTF-8
 
+# vim
 export EDITOR='vim'
 export VISUAL='vim'
+export MY_VIM_CONFIG_DIR="${XDG_CONFIG_HOME}/vim"
+if ! [ -d $MY_VIM_CONFIG_DIR ]; then mkdir -p $MY_VIM_CONFIG_DIR; fi
+export MY_VIM_DATA_DIR="${XDG_DATA_HOME}/vim"
+if ! [ -d $MY_VIM_DATA_DIR ]; then mkdir -p $MY_VIM_DATA_DIR; fi
+export MY_VIM_STATE_DIR="${XDG_STATE_HOME}/vim"
+if ! [ -d $MY_VIM_STATE_DIR ]; then mkdir -p $MY_VIM_STATE_DIR; fi
+
 
 mkdir -p ${XDG_STATE_HOME}/zsh
 HISTFILE=${XDG_STATE_HOME}/zsh/zsh_history
