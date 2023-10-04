b743cfa97393d5dd08e4fe0815acc3dd995c30f1
David Yun <abookyun@gmail.com>
2023-10-04 14:59:33 +0800

    add --paginate flag to git diff alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,8 +10,8 @@ alias gc!='git commit --amend'
 alias gca='git commit -a'
 alias gco='git checkout'
 
-alias gd='git diff --color'
-alias gdc='git diff --cached'
+alias gd='git --paginate diff --color'
+alias gdc='git --paginate diff --cached'
 
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
