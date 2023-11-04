cc9c1bcf347715a4bd227e74a326549ce64d8355
David Yun <abookyun@gmail.com>
2023-11-04 22:30:11 +0800

    ruby: with dotbot, no need to mkdir myself

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -2,8 +2,6 @@ export RBENV_ROOT=$XDG_DATA_HOME/rbenv
 
 RUBY_CONFIG_HOME=$XDG_CONFIG_HOME/ruby
 RUBY_DATA_HOME=$XDG_DATA_HOME/ruby
-if ! [ -d "$RUBY_CONFIG_HOME" ]; then mkdir -p $RUBY_CONFIG_HOME; fi
-if ! [ -d "$RUBY_DATA_HOME" ]; then mkdir -p $RUBY_DATA_HOME; fi
 
 export IRBRC=$RUBY_CONFIG_HOME/irbrc
 export PRYRC=$RUBY_CONFIG_HOME/pryrc
