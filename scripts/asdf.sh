ce7dd01ddee550d1c98aaa76e90c7618986e5a06
David Yun <abookyun@gmail.com>
2023-11-05 22:35:28 +0800

    asdf: add scripts to install asdf plugins in dotbot install

---
+++ scripts/asdf.sh
@@ -0,0 +1,6 @@
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin add python https://github.com/asdf-community/asdf-python.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf plugin add rust https://github.com/code-lever/asdf-rust.git
+asdf install
