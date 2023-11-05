1b9bd850195ca1cf6b1f50d0f52dcc05f7baed01
David Yun <abookyun@gmail.com>
2023-11-05 13:24:50 +0800

    pyenv: fix typo

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -2,4 +2,4 @@ export PYENV_ROOT=$XDG_DATA_HOME/pyenv
 
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 
-if (( $+commands[rbenv] )); then eval "$(pyenv init -)"; fi
+if (( $+commands[pyenv] )); then eval "$(pyenv init -)"; fi
