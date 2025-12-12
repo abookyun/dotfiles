630e2405b347d2fe13cab84b45e3c33be28d299a
David Yun <abookyun@gmail.com>
2025-12-12 12:38:23 +0800

    nvim: add lazy.nvim and plugin configurations
    - lazy.nvim bootstrap in init.lua
    - vim-cool: auto clear search highlight
    - colorscheme: dracula theme
    - telescope: fuzzy finder with leader-f keymaps
    - treesitter: syntax highlighting for multiple languages
    - lsp: nvim-lspconfig for lua, python, ruby

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,20 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lspconfig = require("lspconfig")
+
+    local on_attach = function(client, bufnr)
+      local opts = { buffer = bufnr }
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+    end
+
+    lspconfig.lua_ls.setup({ on_attach = on_attach })
+    lspconfig.pyright.setup({ on_attach = on_attach })
+    lspconfig.ruby_lsp.setup({ on_attach = on_attach })
+  end,
+}
---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+  },
+}
