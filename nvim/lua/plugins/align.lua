05fd16d214e9ec261e67455ae9e280a58482cf38
David Yun <abookyun@gmail.com>
2025-12-12 13:03:19 +0800

    nvim: add utility plugins
    - telescope: add document symbols keymap
    - align: text alignment (replaces tabular)
    - undotree: undo history visualization (kept)

---
+++ nvim/lua/plugins/align.lua
@@ -0,0 +1,7 @@
+return {
+  "echasnovski/mini.align",
+  event = "VeryLazy",
+  config = function()
+    require("mini.align").setup()
+  end,
+}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,5 +7,6 @@ return {
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
     { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+    { "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document symbols" },
   },
 }
