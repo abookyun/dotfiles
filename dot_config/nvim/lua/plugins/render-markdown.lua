3430496fb86d3fcce7b9fbad68edfb46ec8d5b3d
David Yun <abookyun@gmail.com>
2026-09-03 20:09:01 +0800

    feat(nvim): add render-markdown.nvim plugin

---
+++ dot_config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,6 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  ft = { "markdown" },
+  opts = {},
+}
